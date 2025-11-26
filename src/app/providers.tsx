3348e6c26e77a0396b68fa60a83088f38753d34a
chouzzy <mathfernando@hotmail.com>
2025-11-26 15:17:52 -0300

    correção de cores mazzotini

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,27 +1,24 @@
-// src\app\providers.tsx
 "use client"
 
 import system from "@/theme"
-import { ChakraProvider, defaultSystem, Toaster } from "@chakra-ui/react"
+import { ChakraProvider } from "@chakra-ui/react"
 import { ThemeProvider } from "next-themes"
-import { ColorModeProvider } from "./components/ui/color-mode"
 import { Auth0ProviderWithHistory } from "./components/providers/Auth0ProviderWithHistory"
-// import { Toaster } from "@/components/ui/toaster"
-// import { ProfileCompletionGuard } from "./components/auth/ProfileCompletionGuard"
 
-
-export default function RootLayout(props: { children: React.ReactNode }) {
+export default function Providers(props: { children: React.ReactNode }) {
   return (
     <ChakraProvider value={system}>
-      <ColorModeProvider>
-        <ThemeProvider attribute="class" disableTransitionOnChange>
-          <Auth0ProviderWithHistory>
-            {/* <ProfileCompletionGuard> */}
-              {props.children}
-            {/* </ProfileCompletionGuard> */}
-          </Auth0ProviderWithHistory>
-        </ThemeProvider>
-      </ColorModeProvider>
+      <ThemeProvider 
+        attribute="class" 
+        disableTransitionOnChange 
+        defaultTheme="dark"
+        enableSystem={false}
+        forcedTheme="dark"
+      >
+        <Auth0ProviderWithHistory>
+            {props.children}
+        </Auth0ProviderWithHistory>
+      </ThemeProvider>
     </ChakraProvider>
   )
 }
